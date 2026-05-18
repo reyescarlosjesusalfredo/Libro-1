17c290f3d1510b8ef037a5797bc1ee1dd680af4e
reyescarlosjesusalfredo <reyescarlosjesusalfredo@gmail.com>
2026-05-18 08:28:00 -0500

    Add files via upload

---
+++ OperadoresLogicos.java
@@ -0,0 +1,22 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+        System.out.println("Operador AND (&&):");
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+        System.out.println();
+
+        System.out.println("Operador OR (||):");
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println();
+
+        System.out.println("Operador NOT (!):");
+        System.out.println(!true);
+        System.out.println(!false);
+
+    } // Fin del método main
+} // Fin de la clase OperadoresLogicos
