d22d9d21044f3e3daa74e0b463d997cd975c2b5e
reyescarlosjesusalfredo <reyescarlosjesusalfredo@gmail.com>
2026-05-18 08:04:08 -0500

    Add files via upload

---
+++ OperadoresAritmeticos2.java
@@ -0,0 +1,12 @@
+class OperadoresAritmeticos2 {
+    public static void main(String[] args) {
+        double numero1 = 7.0;
+        double numero2 = 5.0;
+        System.out.println("Operaciones con \"numero1\" y \"numero2\":");
+        System.out.println(numero1 + numero2);
+        System.out.println(numero1 - numero2);
+        System.out.println(numero1 * numero2);
+        System.out.println(numero1 / numero2);
+        System.out.println(numero1 % numero2);
+    }
+}
